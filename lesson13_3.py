96ba1636a96e007514fd921ad998ed6526ae610a
SakiHasegawa <136140354+SakiHasegawa@users.noreply.github.com>
2023-08-05 05:52:44 +0000

    modify

--- lesson13_3.py
+++ lesson13_3.py
@@ -1,11 +1,12 @@
+#streamlit run lesson13_3.py
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
-value = st.slider("三角函式",min_value=0,max_value=10)
-t = np.arange(0.,value,0.05)
-y1 = np.sin(2 * np.pi * t)
-y2 = np.cos(2 * np.pi * t)
+value = st.slider("三角函式",min_value=0,max_value=5)
+t = np.arange(0.,5,0.05)
+y1 = np.sin(np.random.randn() * value * np.pi * t)
+y2 = np.cos(np.random.randn()* value * np.pi * t)
 figure1 = plt.figure(figsize=(8,4))
 axes1 = figure1.add_subplot()
 axes1.plot(y1)
